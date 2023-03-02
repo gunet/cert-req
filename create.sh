2227d4811a198843894f2d775eeae9a85af0f302
kkalev <kkalev@gmail.com>
2023-03-02 17:46:07 +0200

    Add an option to self-sign a certificate

--- create.sh
+++ create.sh
@@ -7,7 +7,8 @@ if [[ $# -gt 0 && $1 == "renew" ]]; then
 fi
 if [[ $# -gt 0 && $1 == "create" ]]; then
     echo "Generating server certificate CSR (recreating key).."
-    openssl req -new -newkey rsa:4096 -nodes -keyout certs/privkey.pem -out certs/server.csr -config server.cnf -batch
+    openssl req -new -newkey rsa:4096 -nodes -keyout certs/privkey.pem -out certs/server.csr \
+    -config server.cnf -batch
     exit 0
 fi
 if [[ $# -gt 0 && $1 == "print" ]]; then
@@ -25,9 +26,25 @@ if [[ $# -gt 0 && $1 == "decrypt" ]]; then
     openssl rsa -in certs/privkey.pem -out certs/privkey.pem
     exit 0
 fi
+if [[ $# -gt 0 && $1 == "self-sign" ]]; then
+    echo "Generating private CA key..."
+    openssl genrsa -out certs/cakey.pem 4096
+    echo "Signing CA certificate..."
+    openssl req -x509 -new -nodes -key certs/cakey.pem -sha256 -days 8030 -config ca.cnf \
+    -out certs/ca.crt -extensions ca_ext -batch
+    echo "Generating server certificate CSR..."
+    openssl req -new -newkey rsa:4096 -nodes -keyout certs/privkey.pem -out certs/server.csr -config server.cnf -batch
+    echo "Signing server certificate..."
+    openssl x509 -req -in certs/server.csr -CA certs/ca.crt -CAkey certs/cakey.pem -CAcreateserial \
+    -out certs/server.crt -days 7300 -sha256
+    exit 0
+fi
 
 echo "Usage: $0 <option>"
 echo "Available options:"
 echo "create    Create a new private key and server.csr"
 echo "print     Print CSR"
 echo "renew     Regenerate the CSR reusing the same key"
+echo "encrypt   Encrypt the private key with a passphrase"
+echo "decrypt   Decrypt an encrypted private key"
+echo "self-sign Self sign a certificate"
